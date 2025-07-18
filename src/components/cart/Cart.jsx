c749092cc52b44166488c640a65a0bf259959d1f
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-07-17 17:56:53 -0700

    Refactor Cart component to streamline item linking and selection. Update MainApp to pass setCart prop to Cart for improved state management. Enhance CSS for better styling and hover effects on cart items.

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -9,14 +9,7 @@ export function Cart(props) {
     }
     return (
         <div className={`vertical-flexbox cartContainer`}>
-            {props.cart.map((obj) => 
-            <Link to={`/product/${obj.name}`} onClick={() => {
-                props.setSelectedShoe(obj.item);
-                props.setShowCart(false)
-                localStorage.setItem('selectedShoe', JSON.stringify(obj.item));
-            }} >
-                <CartItems obj={obj} key={obj.item._id} />
-            </Link> )}
+            {props.cart.map((obj) => <CartItems obj={obj} key={obj.item._id} setCart={props.setCart} setShowCart={props.setShowCart} setSelectedShoe={props.setSelectedShoe} />)}
             {props.cart.length === 0 && <div style={{textAlign: 'center'}} >You Haven't Added Anything To The Cart <br /> Go!!! Start Shopping!!!</ div>}
             <div className={`biggest-font-weight ${styles.closeCart}`} onClick={() => props.setShowCart(false)} >X</div>
             {props.cart.length !== 0 && <div className={`horizontal-flexbox ${styles.totalCart}`}>
@@ -30,10 +23,29 @@ export function Cart(props) {
 function CartItems(props) {
     return(
         <div className={`avg-button horizontal-flexbox biggest-font-weight ${styles.cartItem}`}>
-            <div className={`${styles.itemImgContainer}`} data-content={`${props.obj.count}`} ><img src={props.obj.img} alt="" className={`${styles.itemImg}`} /></div>
+                <div className={`avg-button ${styles.itemImgContainer}`} data-content={`${props.obj.count}`} >
+                    <Link to={`/product/${props.obj.name}`} onClick={() => {
+                            props.setSelectedShoe(props.obj.item);
+                            props.setShowCart(false)
+                            localStorage.setItem('selectedShoe', JSON.stringify(props.obj.item));
+                            }}>
+                        <img src={props.obj.img} alt="" className={`${styles.itemImg}`} />
+                    </Link>
+                </div>
             <div className={` horizontal-flexbox ${styles.cartItemInfoContainer}`}>
-                <div>{props.obj.name}</div>
-                <div className={`${styles.cartItemPrice}`} style={{color: 'var(--secondary-color)'}}>${props.obj.price}</div>
+                <Link to={`/product/${props.obj.name}`} onClick={() => {
+                        props.setSelectedShoe(props.obj.item);
+                        props.setShowCart(false)
+                        localStorage.setItem('selectedShoe', JSON.stringify(props.obj.item));
+                    }}>
+                    <div>{props.obj.name}</div>
+                </Link>
+                <div className="vertical-flexbox">
+                    <div className={`${styles.cartItemPrice}`} style={{color: 'var(--secondary-color)'}}>${props.obj.price}</div>
+                    <div className={`${styles.deleteCartItem}`} onClick={() => {props.setCart(prev => { return prev.filter((obj) => obj !== props.obj)})}} >
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" className={`${styles.deleteCartItemSvg}`} width="24px" style={{fill: 'var(--text-color)'}} ><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                    </div>
+                </div>
             </div>
         </div>
     )
